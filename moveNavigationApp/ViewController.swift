418f8d33abe53526b0032b7c7f71ac3d652e864e
Tomonori <tomonori0204.takahashi@gmail.com>
2016-11-02 11:56:24 +0900

    Initial Commit

---
+++ moveNavigationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moveNavigationApp
+//
+//  Created by 高橋知憲 on 2016/11/02.
+//  Copyright © 2016年 高橋知憲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
